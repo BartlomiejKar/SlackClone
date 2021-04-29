927cd5f7b26133839f4cc30bcd5edb6a66aecd71
Bartek Karelus <bartek.karelus@gmail.com>
2021-04-29 20:33:21 +0200

    create modal about add image

--- src/components/Message/Modal.js
+++ src/components/Message/Modal.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import mime from "mime-types";
 import { Modal, Button, Input, Icon } from "semantic-ui-react"
 
-const ModalFile = ({ closeModal, modal }) => {
+const ModalFile = ({ closeModal, modal, uploadFile }) => {
+    const [file, setFile] = useState(null)
+    const acceptedImages = ["image/jpeg", "image/png"]
+
+    const addFile = (e) => {
+        const fileUpload = e.target.files[0];
+        setFile(fileUpload);
+    }
+    const sendFile = () => {
+        if (file !== null) {
+            if (checkFile(file.name)) {
+                const fileType = file.name
+                uploadFile(file, fileType)
+                closeModal();
+                clearFile()
+            }
+        }
+    }
+
+    const checkFile = (file) => acceptedImages.includes(mime.lookup(file))
+
+    const clearFile = () => {
+        setFile(null)
+    }
     return (
         <Modal basic open={modal} onClose={closeModal}>
             <Modal.Header>Select image</Modal.Header>
             <Modal.Content>
                 <Input
+                    onChange={addFile}
                     fluid
                     label="types of file: jpg, png"
                     name="file"
@@ -15,6 +40,7 @@ const ModalFile = ({ closeModal, modal }) => {
             </Modal.Content>
             <Modal.Actions>
                 <Button
+                    onClick={sendFile}
                     color="green"
                     inverted
                 >
